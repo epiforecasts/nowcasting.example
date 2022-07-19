1b1ac59820224fa15e7cfa1b4dbef17a5205b086
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-07-19 07:35:08 +0100

    complete snapshots for correct delays

--- R/utils.r
+++ R/utils.r
@@ -20,30 +20,32 @@ load_data <- function() {
 ##' @param max_delay maximum delay
 ##' @return a list of data frames to feed into `estimate_truncation`
 ##' @importFrom purrr map
-##' @importFrom dplyr arrange count
+##' @importFrom dplyr arrange count pull rename filter
+##' @importFrom tidyr complete
 ##' @author Sebastian Funk
 create_snapshots <- function(x, max_delay) {
   ## remove NA and sort
   x <- x |>
-    filter(!is.na(date_onset), !is.na(report_date),
-           report_date >= date_onset) |>
     dplyr::arrange(report_date, date_onset)
   min_report_date <- min(x$date_onset) + max_delay
   report_dates <- x |>
-    filter(report_date >= min_report_date) |>
-    pull(report_date) |>
+    dplyr::filter(report_date >= min_report_date) |>
+    dplyr::pull(report_date) |>
     unique()
   ## generate snapshots
   snapshots <- purrr::map(
     report_dates,
     ~ x |>
-      filter(report_date <= .x) |>
+      dplyr::filter(report_date <= .x) |>
       dplyr::count(date_onset, name = "confirm") |>
       tidyr::complete(
-        date_onset = seq(min(date_onset), max(date_onset), by = "day"),
+        date_onset = seq(
+          min(date_onset),
+          max(.x, min(date_onset) + max_delay),
+          by = "day"),
         fill =  list(confirm = 0)
       ) |>
-      rename(date = date_onset)
+      dplyr::rename(date = date_onset)
     )
   return(snapshots)
 }

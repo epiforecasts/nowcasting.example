64c536f2320c7bacacdc864abb79dd9653fedbf1
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-11-21 20:12:57 +0000

    flexible creation of snapshots

--- R/utils.r
+++ R/utils.r
@@ -19,35 +19,41 @@ load_data <- function() {
 ##' @title n/a
 ##' @param x data frame containing `report_date` and `date_onset` columns
 ##' @param max_delay maximum delay
+##' @param second name of the secondary (reporting) column
+##' @param first name of the primary (outcome) column
 ##' @return a list of data frames to feed into `estimate_truncation`
 ##' @importFrom purrr map
 ##' @importFrom dplyr arrange count pull rename filter
 ##' @importFrom tidyr complete
+##' @importFrom rlang sym
 ##' @author Sebastian Funk
 ##' @export
-create_snapshots <- function(x, max_delay) {
+create_snapshots <- function(x, max_delay, second = "report_date",
+                             first = "date_onset") {
   ## remove NA and sort
   x <- x |>
-    dplyr::arrange(report_date, date_onset)
-  min_report_date <- min(x$date_onset) + max_delay
+    dplyr::rename(..first = sym(!!first),
+                  ..second = sym(!!second)) |>
+    dplyr::arrange(..second, ..first)
+  min_report_date <- min(x$`..first`) + max_delay
   report_dates <- x |>
-    dplyr::filter(report_date >= min_report_date) |>
-    dplyr::pull(report_date) |>
+    dplyr::filter(..second >= min_report_date) |>
+    dplyr::pull(..second) |>
     unique()
   ## generate snapshots
   snapshots <- purrr::map(
     report_dates,
     ~ x |>
-      dplyr::filter(report_date <= .x) |>
-      dplyr::count(date_onset, name = "confirm") |>
+      dplyr::filter(..second <= .x) |>
+      dplyr::count(..first, name = "confirm") |>
       tidyr::complete(
-        date_onset = seq(
-          min(date_onset),
-          max(.x, min(date_onset) + max_delay - 1),
+        `..first` = seq(
+          min(..first),
+          max(.x, min(..first) + max_delay - 1),
           by = "day"),
-        fill =  list(confirm = 0)
+        fill = list(confirm = 0)
       ) |>
-      dplyr::rename(date = date_onset)
+      dplyr::rename(date = `..first`)
     )
   return(snapshots)
 }
